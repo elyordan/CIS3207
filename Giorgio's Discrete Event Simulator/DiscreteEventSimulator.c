36e223cb0cc7f6da859b9d1d0ba378199357c425
elyordan <elyordan_21@hotmail.com>
2019-09-09 13:00:59 -0400

    First Commit

---
+++ Giorgio's Discrete Event Simulator/DiscreteEventSimulator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+   // printf() displays the string inside quotation
+   printf("\n1 - This is line number one\n2 - This is line number two\n3 - This is line number three\n");
+
+   return 0;
+}
---
+++ Giorgio's Discrete Event Simulator/Readme.md
@@ -0,0 +1,1 @@
+Discrete Event Simulator Homework for CIS3207
