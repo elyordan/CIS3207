54dc58f4f10d49ba565f83d9a04654f1d92025dd
elyordan <elyordan_21@hotmail.com>
2019-09-09 13:06:54 -0400

    Another Coomit test

--- Giorgio's Discrete Event Simulator/DiscreteEventSimulator.c
+++ Giorgio's Discrete Event Simulator/DiscreteEventSimulator.c
@@ -2,8 +2,11 @@
 
 int main() {
 
+   printf("*********************** Trying out another commit test ***********************\n");
+
    // printf() displays the string inside quotation
    printf("\n1 - This is line number one\n2 - This is line number two\n3 - This is line number three\n");
+
 
    return 0;
 }
