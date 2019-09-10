2e982609b62ce5f24f5a1291e1e75cd2cd4bf423
elyordan <elyordan_21@hotmail.com>
2019-09-10 13:47:49 -0400

    added queue files

--- Giorgio's Discrete Event Simulator/DiscreteEventSimulator.c
+++ Giorgio's Discrete Event Simulator/DiscreteEventSimulator.c
@@ -1,12 +1,109 @@
+// Structure for JOBS using typedef:
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-
-   printf("*********************** Trying out another commit test ***********************\n");
-
-   // printf() displays the string inside quotation
-   printf("\n1 - This is line number one\n2 - This is line number two\n3 - This is line number three\n");
-
-
-   return 0;
+ 
+struct Node {
+	int data;
+	struct Node *next;
+};
+ 
+struct Queue {
+	struct Job *front;
+	struct Node *last;
+	unsigned int size;
+};
+ 
+void init(struct Queue *q) {
+	q->front = NULL;
+	q->last = NULL;
+	q->size = 0;
 }
+ 
+int front(struct Queue *q) {
+	return q->front->data;
+}
+ 
+void pop(struct Queue *q) {
+	q->size--;
+ 
+	struct Node *tmp = q->front;
+	q->front = q->front->next;
+	free(tmp);
+}
+ 
+void push(struct Queue *q, int data) {
+	q->size++;
+ 
+	if (q->front == NULL) {
+		q->front = (struct Node *) malloc(sizeof(struct Node));
+		q->front->data = data;
+		q->front->next = NULL;
+		q->last = q->front;
+	} else {
+		q->last->next = (struct Node *) malloc(sizeof(struct Node));
+		q->last->next->data = data;
+		q->last->next->next = NULL;
+		q->last = q->last->next;
+	}
+}
+ 
+// Driver code.
+int main(void) {
+	struct Queue q;
+	init(&q);
+	push(&q, 1);
+	push(&q, 2);
+	printf("%d\n", front(&q));
+	pop(&q);
+	printf("%d\n", front(&q));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+typedef struct {
+
+  int jobId;
+  int jobType;
+  int jobtime;
+
+  struct Job * next;
+  
+} Job;
+
+
+
+
